1eef3cc66317a03a38935804584b41751f9fec92
cruwaller <cruwaller@gmail.com>
2024-04-22 14:45:14 +0300

    NTC thermistor support

--- components/config/include/board_config.h
+++ components/config/include/board_config.h
@@ -160,9 +160,15 @@ typedef struct
     bool wifi_ap_autostart: 1;
     char wifi_ap_ssid[32];
     char wifi_ap_pass[64];
-    
+
     bool addressable_led :1;
     gpio_num_t addressable_led_gpio;
+
+    bool thermistor :1;
+    adc_channel_t thermistor_adc_channel;
+    uint32_t thermistor_r1;
+    uint32_t thermistor_nominal_r;
+    uint32_t thermistor_beta;
 } board_config_t;
 
 extern board_config_t board_config;
